282876395e560bdafca1ff47bb9ee98044774348
陈德华 <130627396+Speidel0402@users.noreply.github.com>
2025-05-09 20:27:50 +0800

    镜像源

--- src/RAG/rag_system.py
+++ src/RAG/rag_system.py
@@ -52,7 +52,8 @@ def clean_text(text):
 # -------- 嵌入模型封装 --------
 class SentenceTransformerEmbeddings(Embeddings):
     def __init__(self, model_name):
-        self.model = SentenceTransformer(model_name, trust_remote_code=True)
+        self.model = SentenceTransformer(model_name, trust_remote_code=True, 
+        model_kwargs={"mirror": "https://mirror.tuna.tsinghua.edu.cn/hugging-face-models"})
 
     def embed_documents(self, texts):
         return self.model.encode(texts, show_progress_bar=True).tolist()
